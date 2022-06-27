b5486fc1ac8efc6f7b20a5e5857ea9d2d74c9b53
Leydi <leydi.madrid6@gmail.com>
2022-06-26 20:47:56 -0500

    Se añade el css al index.html

--- Ejercicio2/index.js
+++ Ejercicio2/index.js
@@ -1,4 +1,12 @@
-const 
+const   a = document.getElementById ('numeroUno'),
+b = document.getElementById ('numeroDos'),
+c = document.getElementById ('numeroTres'),
+d = document.getElementById ('numeroCuatro'),
+boton = document.getElementById ('boton'),
+numMayor = document.getElementById ('resultadoMayor'),
+numMenor = document.getElementById ('resultadoMenor')
+
+boton.addEventListener("click",()=>{  
     a = document.getElementById ('numeroUno'),
     b = document.getElementById ('numeroDos'),
     c = document.getElementById ('numeroTres'),
@@ -6,7 +14,4 @@ const
     boton = document.getElementById ('boton'),
     numMayor = document.getElementById ('resultadoMayor'),
     numMenor = document.getElementById ('resultadoMenor')
-
-boton.addEventListener("blur",()=>{  
-    console.log(boton.value)
 })
